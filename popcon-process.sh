dfa6c029f4e4fc63aa49d51c1174a5838e38a7be
Petter Reinholdtsen <pere@debian.org>
2005-07-05 13:54:55 +0000

    Make it easier to change the directotry structure.

--- popcon-process.sh
+++ popcon-process.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
+
+BASEDIR=/org/popcon.debian.org/popcon-mail
+MAILDIR=../Mail
+WEBDIR=../www
+
 set -e
-cd /org/popcon.debian.org/popcon-mail
+cd $BASEDIR
 umask 0002
 # rotate files
-mv ../Mail/survey new-popcon-entries
-touch ../Mail/survey
-chmod go-rwx ../Mail/survey
+mv $MAILDIR/survey new-popcon-entries
+touch $MAILDIR/survey
+chmod go-rwx $MAILDIR/survey
 
 # process entries
 ./prepop.pl <new-popcon-entries >prepop.out 2>&1
@@ -15,9 +20,9 @@ rm -f results
 find popcon-entries -type f -mtime +20 -print0 | xargs -0 rm -f --
 find popcon-entries -type f | xargs cat \
         | nice -15 ./popanal.py >popanal.out 2>&1
-cp results ../www/all-popcon-results.txt
-gzip -f ../www/all-popcon-results.txt
-cp ../www/all-popcon-results.txt.gz all-popcon-results/popcon-`date +"%Y-%m-%d"`.gz
+cp results $WEBDIR/all-popcon-results.txt
+gzip -f $WEBDIR/all-popcon-results.txt
+cp $WEBDIR/all-popcon-results.txt.gz all-popcon-results/popcon-`date +"%Y-%m-%d"`.gz
 cd ../popcon-stat
 
 find ../popcon-mail/all-popcon-results -type f -print | sort |./popcon-stat.pl
