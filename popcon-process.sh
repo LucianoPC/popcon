d7f89667b4eed53162002996335d921bf9b53b3f
Petter Reinholdtsen <pere@debian.org>
2005-07-23 23:25:08 +0000

    Make email processing optional.

--- popcon-process.sh
+++ popcon-process.sh
@@ -8,23 +8,31 @@ BINDIR=$BASEDIR/../bin
 DATADIR=$BASEDIR/popcon-entries
 SUMMARYDIR=$BASEDIR/all-popcon-results
 
+# set to 'true' if email submissions should be processed
+READMAIL=true
+
 # Remove entries older than # number of days
 DAYLIMIT=20
 
 set -e
 cd $BASEDIR
 umask 0002
-# rotate files
-mv $MAILDIR/survey new-popcon-entries
-touch $MAILDIR/survey
-chmod go-rwx $MAILDIR/survey
 
-# process entries
-$BINDIR/prepop.pl <new-popcon-entries >$LOGDIR/prepop.out 2>&1
+# rotate incoming mail spool files
+if [ true = "$READMAIL" ] ; then
+    mv $MAILDIR/survey new-popcon-entries
+    touch $MAILDIR/survey
+    chmod go-rwx $MAILDIR/survey
+
+    # process entries, splitting them into individual reports
+    $BINDIR/prepop.pl <new-popcon-entries >$LOGDIR/prepop.out 2>&1
+fi
 
 # delete outdated entries
 rm -f results
 find $DATADIR -type f -mtime +$DAYLIMIT -print0 | xargs -0 rm -f --
+
+# Generate statistics
 find $DATADIR -type f | xargs cat \
         | nice -15 $BINDIR/popanal.py >$LOGDIR/popanal.out 2>&1
 cp results $WEBDIR/all-popcon-results.txt
