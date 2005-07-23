ee136fda047542fc79c5082acae73e71ddc7c422
Petter Reinholdtsen <pere@debian.org>
2005-07-23 23:07:08 +0000

    Make the timeout value more obvoius.

--- popcon-process.sh
+++ popcon-process.sh
@@ -7,6 +7,9 @@ LOGDIR=$BASEDIR/../logs
 BINDIR=$BASEDIR/../bin
 SUMMARYDIR=$BASEDIR/all-popcon-results
 
+# Remove entries older than # number of days
+DAYLIMIT=20
+
 set -e
 cd $BASEDIR
 umask 0002
@@ -20,7 +23,7 @@ $BINDIR/prepop.pl <new-popcon-entries >$LOGDIR/prepop.out 2>&1
 
 # delete outdated entries
 rm -f results
-find popcon-entries -type f -mtime +20 -print0 | xargs -0 rm -f --
+find popcon-entries -type f -mtime +$DAYLIMIT -print0 | xargs -0 rm -f --
 find popcon-entries -type f | xargs cat \
         | nice -15 $BINDIR/popanal.py >$LOGDIR/popanal.out 2>&1
 cp results $WEBDIR/all-popcon-results.txt
